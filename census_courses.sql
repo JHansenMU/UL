a242fe9a2629e9189c7ad0aabb6682acf1a7bb5c
John Hansen <jhansen@missouri.edu>
2026-01-19 15:48:32 -0600

    Finish test file with 3 students.

--- census_courses.sql
+++ census_courses.sql
@@ -50,7 +50,7 @@ WHERE strm = '5343'         -- 5343 = FS25
     AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
 --    AND emplid = '14509672'
 ORDER BY ADMIT_TERM DESC
-FETCH FIRST 30 ROWS ONLY;
+FETCH FIRST 50 ROWS ONLY;
 
 -- --- --- --- 
 -- Build out Query to list courses for one individual
@@ -78,6 +78,7 @@ AND a.STRM = '5343' -- FS25
 -- A subset of this table could be used to show when students change from BUSNU to JOURN or other.
 -- We now need to get a list of EMPLID's for the business students of interest. Perhaps use
 -- the census table and STRM current semester and  AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
+-- Need to build in 'ADMIT_TERM' from census table or other location.
 SELECT a.EMPLID
      , a.strm
      , c.term
@@ -103,6 +104,7 @@ FROM  sa_c.sa_stdnt_enrl_Fc a
     , sa_c.sa_class_dm b
     , mu_sis_mv.students_active_in_prog_by_term_mv c
     , sa_c.ps_um_census_enrl d
+    , um_sdsc.mu_4d_adm_appl_dtl_wk0 e
 WHERE
         a.stdnt_enrl_status = 'E'     -- These three col required
     and a.enrl_status_reason = 'ENRL' -- to get enrolled courses
@@ -117,15 +119,20 @@ WHERE
     and a.strm = d.strm               -- Link to UM_CLEVEL_DESCR,
     and a.emplid = d.emplid           -- ACAD_PLAN etc.
     
-    AND a.emplid IN (
-        SELECT distinct emplid 
-        FROM sa_c.ps_um_census_enrl 
-        WHERE strm = '5343' 
-        AND acad_career = 'UGRD'
-        AND um_deg_seeking = 'Y' -- Census includes non degree seeking students.
-        AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
-        AND um_clevel_descr IN('FRESHMAN', 'SOPHOMORE', 'JUNIOR', 'SENIOR') --n=1,337
-        )
+    and a.strm = e.strm               -- Link to UM_CLEVEL_DESCR,
+    and a.emplid = e.emplid           -- ACAD_PLAN etc.
+    
+-- Below subquery returns full set of census EMPLID for semester of interest.
+--    AND a.emplid IN (                 -- Per request from Shannon B use census
+--        SELECT distinct emplid 
+--        FROM sa_c.ps_um_census_enrl 
+--        WHERE strm = '5343' 
+--        AND acad_career = 'UGRD'
+--        AND um_deg_seeking = 'Y' -- Census includes non degree seeking students.
+--        AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
+--        AND um_clevel_descr IN('FRESHMAN', 'SOPHOMORE', 'JUNIOR', 'SENIOR') --n=1,337 for F, n=5226 for F,S,J,S
+--        )
+        
 --    and a.emplid IN -- Subquery returns n=5,230 emplids
 --    and see the subquery directly below. This will build a set of all Trulaske
 -- students with each row being a class they have taken during their entire career
@@ -136,6 +143,7 @@ WHERE
 --and a.emplid IN('14467584', '14466324') -- FRESHMAN
 --and a.emplid IN('14467078', '14509796') -- FRESHMAN start FS25
 -- and a.emplid IN('14467581', '14466268') -- SOPHOMORE
+-- and a.emplid IN('14467581', '14466268', '14467014') -- SOPHOMORE
 -- and a.emplid = '14467602' -- JUNIOR
 
 --AND a.STRM IN('5343', '5327') -- FS25, SP25
@@ -180,6 +188,191 @@ and emplid IN('14467581', '14466268') -- SOPHOMORE
 FETCH FIRST 20 ROWS ONLY;
 
 
+-- Get Table columns um_sdsc.mv_4d_adm_appl_dtl_wk0
+--Pull admissions data out for FTC and TRE seperately here
+  select distinct e.admit_term_rollup, e.admit_term_rollup_descrshort, e.emplid, 
+  e.admit_type, e.um_deg_seeking, 
+  e.ever_admit, e.is_deposit_paid, e.is_cancel
+
+  from um_sdsc.mu_4d_adm_appl_dtl_wk0 e
+  where 
+--  e.admit_term_rollup = '5343' -- Fall 2025 = 5343
+   e.um_deg_seeking = 'Y'
+  and e.acad_career = 'UGRD'
+  and e.admit_type in ('FTC','TRE')
+  and e.acad_group = 'CBUSN'
+  and a.emplid IN('14467581', '14466268', '14509796') -- SOPHOMORE
+  FETCH FIRST 20 ROWS ONLY;
+
+
+        SELECT distinct emplid 
+        FROM sa_c.ps_um_census_enrl 
+        WHERE strm = '5343' 
+        AND acad_career = 'UGRD'
+        AND um_deg_seeking = 'Y' -- Census includes non degree seeking students.
+        AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
+        AND um_clevel_descr IN('FRESHMAN');, 'SOPHOMORE', 'JUNIOR', 'SENIOR');
+
+
+-- --- TEST with Inner join --
+SELECT a.EMPLID
+     , a.strm
+     , c.term
+     , a.last_upd_dt_stmp
+     , d.um_clevel_descr  -- Freshman, Sophomore, etc.
+     , a.acad_prog        -- BUSNU
+     , c.acad_plan        -- BUSAD_BSBA
+     , c.acad_subplan     -- FIN_BANK
+     , b.subject
+     , b.catalog_nbr
+     , b.class_nbr
+     , a.crse_grade_input
+     , a.grd_pts_per_unit
+     , a.unt_taken
+     , c.unt_taken_prgrss
+     , a.grade_points
+     , c.cum_gpa
+     , c.tot_cumulative    -- Assume total hours taken and in progress.
+--     , c.tot_passd_prgrss
+     , MAX(c.tot_cumulative) OVER (
+        PARTITION BY a.emplid)      AS tot_hrs_life 
+FROM  sa_c.sa_stdnt_enrl_Fc a 
+    , sa_c.sa_class_dm b
+    , mu_sis_mv.students_active_in_prog_by_term_mv c
+    , sa_c.ps_um_census_enrl d
+--    , um_sdsc.mu_4d_adm_appl_dtl_wk0 e
+WHERE
+        a.stdnt_enrl_status = 'E'     -- These three col required
+    and a.enrl_status_reason = 'ENRL' -- to get enrolled courses
+    and a.grading_basis_enrl <> 'NON' -- 
+
+    and a.class_nbr = b.class_nbr     -- Link to class table
+    and a.strm = b.strm               -- for classes by term.
+    
+    and a.strm = c.strm               -- Link to CUM_GPA, hours,
+    and a.emplid = c.emplid           -- ACAD_PLAN etc.
+    
+    and a.strm = d.strm               -- Link to UM_CLEVEL_DESCR,
+    and a.emplid = d.emplid           -- ACAD_PLAN etc.
+        
+-- Below subquery returns full set of census EMPLID for semester of interest.
+    AND a.emplid IN (                 -- Per request from Shannon B use census
+        SELECT distinct emplid 
+        FROM sa_c.ps_um_census_enrl 
+        WHERE strm = '5343' 
+        AND acad_career = 'UGRD'
+        AND um_deg_seeking = 'Y' -- Census includes non degree seeking students.
+        AND um_acad_grp_descr = 'College of Business' --  for just Trulaske
+--        AND um_clevel_descr IN('FRESHMAN', 'SOPHOMORE', 'JUNIOR', 'SENIOR') --n=1,337 for F, n=5226 for F,S,J,S
+        and emplid IN('14467078', '14466324') -- FRESHMAN
+        )
+        
+--    and a.emplid IN -- Subquery returns n=5,230 emplids
+--    and see the subquery directly below. This will build a set of all Trulaske
+-- students with each row being a class they have taken during their entire career
+-- from freshmen to seniors. as many as 40 classes  times 5230 students = 209,200 max
+ 
+--and a.emplid = '14467956' -- Freshman transfered out after first Semester
+--and a.emplid IN('14467584') -- FRESHMAN
+--and a.emplid IN('14467584', '14466324') -- FRESHMAN
+--and a.emplid IN('14467078', '14509796') -- FRESHMAN start FS25
+-- and a.emplid IN('14467581', '14466268') -- SOPHOMORE
+-- and a.emplid IN('14467581', '14466268', '14467014') -- SOPHOMORE
+-- and a.emplid = '14467602' -- JUNIOR
+
+--AND a.STRM IN('5343', '5327') -- FS25, SP25
+--AND a.STRM IN('5343', '5427') -- FS25, SP26
+--AND a.STRM IN('5343') -- FS25
+ORDER BY a.strm;
+
+-- First test = =29 rows
+
+-- QUERY REWRITE with inner join -- Use this for the final version
+WITH Business_Students AS (
+    -- This small subset drives the performance of the entire query
+    SELECT DISTINCT emplid 
+    FROM sa_c.ps_um_census_enrl 
+    WHERE strm = '5343'         -- The controling term for entire process.
+      AND acad_career = 'UGRD'
+      AND um_deg_seeking = 'Y'
+      AND um_acad_grp_descr = 'College of Business'
+      AND emplid IN ('14467078', '14466324', '14467014', '14467602') -- Remove for full set.
+)
+SELECT 
+    a.emplid,
+    a.strm,
+    c.term,
+    -- Admission Fields (Table E)
+    e.admit_term_rollup AS ADMIT_TERM, 
+    e.admit_term_rollup_descrshort ADMIT_TERM_DESC, 
+--    e.ever_admit, 
+--    e.is_deposit_paid, 
+--    e.is_cancel,
+    a.last_upd_dt_stmp,
+    d.um_clevel_descr,
+    a.acad_prog,
+    c.acad_plan,
+    c.acad_subplan,
+    b.subject,
+    b.catalog_nbr,
+    b.class_nbr,
+    a.crse_grade_input,
+    a.grd_pts_per_unit,
+    a.unt_taken,
+    c.unt_taken_prgrss,
+    a.grade_points,
+    c.cum_gpa,
+    c.tot_cumulative,
+    MAX(c.tot_cumulative) OVER (PARTITION BY a.emplid) AS tot_hrs_life 
+FROM sa_c.sa_stdnt_enrl_Fc a
+-- The Inner Join below restricts 'a' to only the Business students immediately
+-- Filter 'a' immediately by our driving list
+INNER JOIN Business_Students bus 
+    ON a.emplid = bus.emplid
+-- Join the Admission Table (Table E)
+INNER JOIN um_sdsc.mu_4d_adm_appl_dtl_wk0 e
+    ON a.emplid = e.emplid
+--    AND a.strm = e.admit_term_rollup
+INNER JOIN sa_c.sa_class_dm b 
+    ON a.class_nbr = b.class_nbr 
+    AND a.strm = b.strm
+INNER JOIN mu_sis_mv.students_active_in_prog_by_term_mv c 
+    ON a.strm = c.strm 
+    AND a.emplid = c.emplid
+INNER JOIN sa_c.ps_um_census_enrl d 
+    ON a.strm = d.strm 
+    AND a.emplid = d.emplid
+WHERE a.stdnt_enrl_status = 'E'
+  AND a.enrl_status_reason = 'ENRL'
+  AND a.grading_basis_enrl <> 'NON'
+  -- Filter Table E to ensure we get the correct "Entry" record
+  AND e.um_deg_seeking = 'Y'
+  AND e.acad_career = 'UGRD'
+  AND e.admit_type IN ('FTC','TRE')
+  AND e.acad_group = 'CBUSN'
+ORDER BY a.emplid, a.strm;
+-- n-29 rows
+-- --- END TEST with Inner join --
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT *
+FROM sa_c.sa_stdnt_enrl_Fc
+WHERE STRM = '5243'
+and emplid = '14467602' -- JUNIOR
+FETCH FIRST 20 ROWS ONLY;
+--and a.emplid IN('14467078', '14509796') -- FRESHMAN start FS25
+-- and a.emplid IN('14467581', '14466268') -- SOPHOMORE
 
 
 
